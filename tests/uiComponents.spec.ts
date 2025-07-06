00995f3db69639169fd419a4800b940f05a28bcd
nhanvo <huunhan.vo.5@gmail.com>
2025-07-07 00:06:58 +0700

    add more test

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -80,6 +80,30 @@ test('list and dropdown', async ({ page }) => {
     if (color !== 'Corporate') 
       await dropdownMenu.click()
   } 
+})
+
+test('tooltips', async ({ page }) => {
+  await page.getByText('Modal & Overlays').click()
+  await page.getByText('Tooltip').click()
+
+  const toolTipCard = page.locator('nb-card', { hasText: 'Tooltip Placements' })
+  await toolTipCard.getByRole('button', { name: 'Top' }).hover()
+
+  page.getByRole('tooltip') // if you have a role tooltip created
+  const tooltip = await page.locator('nb-tooltip').textContent()
+  expect(tooltip).toEqual('This is a tooltip')
 
 })
 
+test('dialog boxes', async ({ page }) => {
+  await page.getByText('Tables & Data').click()
+  await page.getByText('Smart Table').click()
+
+  page.on('dialog', dialog => {
+    expect(dialog.message()).toEqual('Are you sure you want to delete?')
+    dialog.accept()
+  })
+
+  await page.getByRole('table').locator('tr', {hasText: "mdo@gmail.com"}).locator('.nb-trash').click()
+  await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
+})
