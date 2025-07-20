630bd9f49c7fa24b8f7a57d25e44c3cb94768467
nhanvo <huunhan.vo.5@gmail.com>
2025-07-20 22:49:15 +0700

    update on Jul 20

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -107,3 +107,99 @@ test('dialog boxes', async ({ page }) => {
   await page.getByRole('table').locator('tr', {hasText: "mdo@gmail.com"}).locator('.nb-trash').click()
   await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
 })
+
+test('web tables', async ({ page }) => {
+  await page.getByText('Tables & Data').click()
+  await page.getByText('Smart Table').click()
+
+  // get the row by any text in this row
+  const targetRow = page.getByRole('row', { name: 'twitter@outlook.com' })
+  await targetRow.locator('.nb-edit').click()
+  await page.locator('input-editor').getByPlaceholder('Age').clear()
+  await page.locator('input-editor').getByPlaceholder('Age').fill('35')
+  await page.locator('.nb-checkmark').click()
+
+  // select a row by id column
+  await page.locator('.ng2-smart-pagination-nav').getByText('2') .click()
+  const targetRowById = page.getByRole('row', { name: '11' }).filter({ has: page.locator('td').nth(1).getByText('11')})
+  await targetRowById.locator('.nb-edit').click()
+  await page.locator('input-editor').getByPlaceholder('E-mail').clear()
+  await page.locator('input-editor').getByPlaceholder('E-mail').fill('huunhan.vo@gmail.com')
+  await page.locator('.nb-checkmark').click()
+  await expect(targetRowById.locator('td').nth(5)).toHaveText('huunhan.vo@gmail.com')
+
+  // test filter of the table
+  const ages = ["20", "30", "40", "200"]
+  for(let age of ages) {
+    await page.locator('input-filter').getByPlaceholder('Age').clear()
+    await page.locator('input-filter').getByPlaceholder('Age').fill(age)
+    await page.waitForTimeout(500) // wait for the table to filter
+    const rows = page.locator('tbody tr')
+    for(let row of await rows.all()) {
+      const ageText = await row.locator('td').last().textContent()
+      if (age=="200")
+      {
+        expect(await page.getByRole('table').textContent()).toContain('No data found')  }
+      else
+      {
+      expect(ageText).toEqual(age)
+    }
+    }   
+  }
+})
+
+test('date pickers', async ({ page }) => {
+  await page.getByText('Forms').click()
+  await page.getByText('Datepicker').click()
+
+  const calendarInputField = page.getByPlaceholder('Form Picker')
+  await calendarInputField.click()
+
+  let date = new Date()
+  date.setDate(date.getDate() + 200) // tomorrow
+  const expectedDate = date.getDate().toString()
+  const expectedMonthShort = date.toLocaleString('En-US', { month: 'short' })
+  const expectedMonthLong = date.toLocaleString('En-US', { month: 'long' })
+  const expectedYear = date.getFullYear()
+  const expectedDateText = `${expectedMonthShort} ${expectedDate}, ${expectedYear}`
+  console.log(expectedDateText)
+
+  let calendarMonthAndyear = await page.locator('nb-calendar-view-mode').textContent()
+  const expectedMonthAndYear = ` ${expectedMonthLong} ${expectedYear} `
+  while(!calendarMonthAndyear.includes(expectedMonthAndYear)){
+    await page.locator('nb-calendar-pageable-navigation [data-name ="chevron-right"]').click()
+    calendarMonthAndyear = await page.locator('nb-calendar-view-mode').textContent()
+  }
+
+  await page.locator('[class="day-cell ng-star-inserted"]').getByText(expectedDate, {exact: true}).click()
+  await expect(calendarInputField).toHaveValue(expectedDateText)
+})
+
+test('sliders', async ({ page }) => {
+  // // update attrribute
+  // const tempGauge = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger circle')
+  // await tempGauge.evaluate( node => {
+  //   node.setAttribute('cx', '232.630')
+  //   node.setAttribute('cy', '232.630')
+  // })
+  // await tempGauge.click()
+
+  // Mouse movement
+  const tempBox = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger')
+  await tempBox.scrollIntoViewIfNeeded()
+
+  const box = await tempBox.boundingBox()
+  const x = box.x + box.width / 2
+  const y = box.y + box.height / 2
+  await page.mouse.move(x, y)
+  await page.mouse.down()
+  await page.mouse.move(x + 100, y)
+  await page.mouse.move(x + 100, y + 100)
+  await page.mouse.up()
+
+  await expect(tempBox).toContainText('30 Celsius')
+})
+
+test('Drag & Drop with iframe', async ({ page }) => {
+
+})
